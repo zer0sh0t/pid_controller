bbb24ed55e4229953318d23c495068b76b2c905d
kumar-harin <haryn.k10@gmail.com>
2022-01-26 22:36:09 +0530

    updating robot model

--- robot_motion_with_pid.cpp
+++ robot_motion_with_pid.cpp
@@ -1,4 +1,4 @@
-#include "time.h"
+#include <time.h>
 #include <iostream>
 #include "pid/pid_controller.h"
 
@@ -9,9 +9,15 @@ class Robot
 
     Robot() { this->position = 0.0; } // robot's initial position
 
-    double move(double velocity)
+    double move(double current, double dt)
     {
-        this->position += velocity * 2.0;
+        double torque = 0.5 * current;
+        double velocity = 3.0 * torque;
+        this->position += velocity * dt;
+
+        srand(time(0));
+        int num = (rand() % (10 - 0 + 1)) + 0;
+        if (num > 5) { this->position += this->position * 0.0001; } // non-linearity
         return this->position;
     }
 };
@@ -21,9 +27,9 @@ int main()
     Robot robot;
     double curr_position = robot.position;
 
-    double Kp_ = 5.0;
-    double Ki_ = 2.0;
-    double Kd_ = 3.0;
+    double Kp_ = 1.0;
+    double Ki_ = 0.7;
+    double Kd_ = 0.0;
     double set_point_ = 20.0; // we want to move the robot to this position
     double sample_time_ = 4.0;
     double clamp_vals_[] = {-100, 100};
@@ -37,8 +43,8 @@ int main()
         double curr_time = time(NULL);
         double dt = curr_time - last_time;
 
-        double velocity = pid_ctrl.update(curr_position);
-        curr_position = robot.move(velocity);
+        double current = pid_ctrl.update(curr_position);
+        curr_position = robot.move(current, 2); // here dt = 2
         last_time = curr_time;
     }
     std::cout << "final position of the robot is " << robot.position << std::endl;
